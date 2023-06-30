8679e7c347af5739a0f5c240ee70d5682f48f2f5
BntngSantosa <bintangsantosa11@gmail.com>
2023-06-30 15:12:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# rock-paper-scissors-Game
+This is a simple web-based rock-paper-scissors game developed using HTML, CSS, and JavaScript. Players can play against the computer and keep track of their scores. The game interface is user-friendly and responsive.
+
+Getting Started
+To play the game, follow these steps:
+
+Clone the repository to your local machine:
+Copy code
+git clone https://github.com/your-username/rock-paper-scissors-Game
+.git
+Open the index.html file in your preferred web browser.
+
+Start playing the game by clicking on the images representing rock, paper, or scissors. The computer will randomly select its choice, and the result will be displayed on the screen.
+
+The score will be updated based on the game's outcome: win, lose, or draw.
+
+Features
+Play rock-paper-scissors against the computer.
+Keep track of the player's and computer's scores.
+Reset the game to start a new match.
+Technologies Used
+HTML
+CSS
+JavaScript
+Font Awesome (for icons)
+Google Fonts (Ubuntu font)
+
+Screenshots
+Game Interface
+![Deskripsi Gambar](asets/img/ui.png)
+License
+This project is licensed under the MIT License.
+
+Acknowledgments
+The project was created as a fun learning exercise and is not intended for commercial use.
+
+Enjoy the game!
---
+++ src/js/script.js
@@ -0,0 +1,86 @@
+function pilihanCOm() {
+  const comp = Math.random();
+  if (comp < 0.34) return "kertas";
+  if (comp >= 0.34 && comp < 0.67) return "batu";
+  return "gunting";
+}
+
+function getHasil(comp, player) {
+  if (player == comp) return "SERI";
+  if (player == "kertas") return comp == "batu" ? "MENANG!" : "KALAH!";
+  if (player == "batu") return comp == "kertas" ? "KALAH!" : "MENANG!";
+  if (player == "gunting") return comp == "batu" ? "KALAH!" : "MENANG!";
+}
+
+const getScore = (hasil) => {
+  const notifWon = document.querySelector(".won-none");
+  const notifLost = document.querySelector(".lost-none");
+  const notifDraw = document.querySelector(".draw-none");
+  const scorePlayer = document.querySelector(".player");
+  const scoreComp = document.querySelector(".computer");
+  let scoreP = parseFloat(scorePlayer.textContent);
+  let scoreC = parseFloat(scoreComp.textContent);
+
+  if (hasil == "MENANG!") {
+    const btnOk = document.querySelector(".btn-ok-won");
+    notifWon.setAttribute("class", "won");
+    btnOk.addEventListener("click", () => {
+      notifWon.setAttribute("class", "won-none");
+      scoreP += 1;
+      scorePlayer.innerHTML = scoreP;
+    });
+  } else if (hasil == "KALAH!") {
+    const btnOk = document.querySelector(".btn-ok-lost");
+    notifLost.setAttribute("class", "lost");
+    btnOk.addEventListener("click", () => {
+      notifLost.setAttribute("class", "lost-none");
+      scoreC += 1;
+      scoreComp.innerHTML = scoreC;
+    });
+  } else {
+    const btnOk = document.querySelector(".btn-ok-draw");
+    notifDraw.setAttribute("class", "draw");
+    btnOk.addEventListener("click", () => {
+      notifDraw.setAttribute("class", "draw-none");
+    });
+  }
+};
+
+function acakImg() {
+  const imgComp = document.querySelector(".imgs-comp");
+  const img = ["kertas", "batu", "gunting"];
+  const timeStart = new Date().getTime();
+  let i = 0;
+  setInterval(() => {
+    if (new Date().getTime() - timeStart > 1000) return clearInterval;
+    imgComp.setAttribute("src", "asets/img/" + img[i++] + ".png");
+    if (i == img.length) i = 0;
+  }, 100);
+}
+
+const imgPil = document.querySelectorAll(".imgs img");
+imgPil.forEach((pil) => {
+  pil.addEventListener("click", () => {
+    const pilComp = pilihanCOm();
+    const pilPlayer = pil.className;
+    const hasil = getHasil(pilComp, pilPlayer);
+    acakImg();
+    setTimeout(() => {
+      const imgComp = document.querySelector(".imgs-comp");
+      imgComp.setAttribute("src", "asets/img/" + pilComp + ".png");
+      getScore(hasil);
+    }, 1000);
+  });
+});
+
+function reset() {
+  const btnReset = document.querySelector(".btn-reset");
+  const scorePlayer = document.querySelector(".player");
+  const scoreComp = document.querySelector(".computer");
+
+  btnReset.addEventListener("click", () => {
+    scorePlayer.innerHTML = "0";
+    scoreComp.innerHTML = "0";
+  });
+}
+reset();
